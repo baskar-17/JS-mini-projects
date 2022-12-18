c4283e8c3089f61bd2852604bc610756e2d6b9a8
Baskar <baskar.official@outlook.in>
2022-12-18 19:09:49 +0000

    bugs fixed

--- app.js
+++ app.js
@@ -3,28 +3,22 @@
 // Feedback Card
 
 function yes() {
-  let feedbackHeading = (document.getElementById('feedbackHeading').innerText =
-    'Thanks for attending today class 🎉');
-  let feedbackDesc = (document.getElementById('feedbackDesc').innerText =
-    'Your are on track, We appriciate your participation.');
-  let feedbackPriBtn = (document.getElementById(
-    'feedbackPriBtn'
-  ).style.display = 'none');
-  let feedbackSecBtn = (document.getElementById(
-    'feedbackSecBtn'
-  ).style.display = 'none');
+  let feedbackHeading = document.getElementById('feedbackHeading');
+  feedbackHeading.innerText = 'Thanks for attending yesterday class 🎉';
+  let feedbackDesc = document.getElementById('feedbackDesc');
+  feedbackDesc.innerText =
+    'Your are on track, We appriciate your participation.';
+  document.getElementById('feedbackPriBtn').style.display = 'none';
+  document.getElementById('feedbackSecBtn').style.display = 'none';
 }
 function no() {
-  let feedbackHeading = (document.getElementById('feedbackHeading').innerText =
-    'Its Okay, we will meet you in the upcoming classes 🍃');
-  let feedbackDesc = (document.getElementById('feedbackDesc').innerText =
-    'Please watch the recordings of missed classes.');
-  let feedbackPriBtn = (document.getElementById(
-    'feedbackPriBtn'
-  ).style.display = 'none');
-  let feedbackSecBtn = (document.getElementById(
-    'feedbackSecBtn'
-  ).style.display = 'none');
+  let feedbackHeading = document.getElementById('feedbackHeading');
+  feedbackHeading.innerText =
+    'Its Okay, we will meet you in the upcoming classes 🍃';
+  let feedbackDesc = document.getElementById('feedbackDesc');
+  feedbackDesc.innerText = 'Please watch the recordings of missed classes.';
+  document.getElementById('feedbackPriBtn').style.display = 'none';
+  document.getElementById('feedbackSecBtn').style.display = 'none';
 }
 
 // Cube Calculation
@@ -32,7 +26,7 @@ function no() {
 function cube() {
   let number = document.getElementById('cubeNumber').value;
   let result = number * number * number;
-  userValue.innerText = number + '³';
+  userValue.innerText = number + '³' + ' ' + 'is' + ' ';
   cubeResult.innerText = result;
 }
 
@@ -42,7 +36,7 @@ function bill() {
   let amount = document.getElementById('billAmount').value;
   let person = document.getElementById('billPerson').value;
   let result = (amount / person).toFixed(2);
-  billResult.innerText = '₹' + result;
+  billResult.innerText = '₹' + ' ' + result;
 }
 
 // Medium Additoin function
